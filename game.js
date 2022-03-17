3425a1b0170e573f92dcd833121702ca1327b360
AlexGHcrhs <>
2022-03-17 12:18:59 +0000

    Add Button Input & Input Function

--- game.js
+++ game.js
@@ -1,26 +1,44 @@
 // Gets the text element for the game from the HTML document
 const textElement = document.getElementById('text');
 
-// Changes the string value of textElement
-textElement.innerText = "Videogaming";
-
 let timeline = 1; // Current textNode or 'room', begins at 1
 
-const choices = ['a: ', 'b: ', 'c: ', 'd: ', 'e: ']; // Theres gotta be a better way to do this
+const choices = ['a', 'b', 'c', 'd']; // Theres gotta be a better way to do this
 
 // Displays a node of the game and asks a set of questions
 function ask(id) {
     console.log('Current node: ' + id);
+    // Find the current textNode object
     const textNode = textNodes.find(textNode => textNode.id === id);
+  
     let combinedText = textNode.text + '\n';
     // Format and concatenate each of the options text to combinedText
     for (let i=0;i<textNode.options.length;i++) {
-        combinedText+=choices[i]+textNode.options[i].text;
+        combinedText+=choices[i]+": "+textNode.options[i].text;
         combinedText+='\n';
     }
     console.log(combinedText);
     textElement.innerText = combinedText;
 }
+
+// Fires whenever a choice button is pressed, choice is an integer which acts as an index for textNode.options[]
+function buttonPress(choice) {
+  textElement.innerText+="\n: "+choices[choice];
+
+  // Finds textNode again (inefficient)
+  const textNode = textNodes.find(textNode => textNode.id === timeline);
+  
+  if (textNode.options[choice] === undefined) {
+    console.log("Invalid Option Entered: "+choices[choice]);
+  } else {
+    textElement.innerText+="\n"+textNode.options[choice].result;
+    console.log("Next node: "+textNode.options[choice].next);
+  }
+
+  // Function still needs to transition to the next node so the program can continue to run as the user inputs many responses
+}
+
+
 
 // All the stored text of the game, all options and outcomes, and all item interactions
 const textNodes = [
@@ -36,10 +54,10 @@ const textNodes = [
             {
                 text: 'n o',
                 result: 'lame',
-                next: 'end' // End will probably have a specific value common to all deaths, possibly 0
+                next: 0
             }
         ]
     }
 ]
 
-ask(1);
+ask(timeline);
