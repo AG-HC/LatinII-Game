4a5b5f30cb829c00b1e21b5203756a81512da3a4
AG-HC <agarcia2@hcrhs.org>
2022-04-23 12:52:01 -0400

    1.2

---
+++ README.md
@@ -0,0 +1,36 @@
+# LatinII-Game
+HTML and JS text-based adventure game based on the Herculean Trials
+
+# Changelog
+
+## 1.2
+* Added Trials 3 and 4
+* Modified handler for changing trials
+* Temporairily increased health to 5 for testing purposes
+* Fixed health display not updating on trial change
+* Minor game code adjustments
+* If game not done, game undon'st
+
+### 1.1b
+* Randomly selected damage flavortext added
+* Minor trial 1 JSON bugfix
+* CSS spacing tweak
+* RomaEstInItalia
+
+### 1.1a
+* Re-implemented health system
+* Minor health system adjustments
+* lionâs hide
+
+## Alpha 1.1
+* Added Trial 2
+* Added styles test page
+* Added 2 alternative styles (light and rome)
+* JSON reading changes (function)
+* Minor game text spacing adjustments
+
+## Alpha 1.0
+* Finished up to end of Trial 1
+* Added JSON compatibility (in-line, currently cannot read file through js)
+* Renamed game (The Legend of Hercules: Breath of the Twine)
+* Health disabled
--- game.js
+++ game.js
@@ -3,7 +3,8 @@ const textElement = document.getElementById('text');
 const healthElement = document.getElementById('healthbar');
 
 let timeline = 1; // Current textNode or 'room', begins at 1
-let health = 3;
+let health = 5;
+let currentTrial = 1;
 
 const letters = ['a', 'b', 'c', 'd'];
 // Go wild
@@ -24,13 +25,13 @@ function ask(id) {
   console.log(combinedText);
   textElement.innerText += combinedText;
 
-  currentNode = textNodes.find(textNode => textNode.id === timeline);
+  currentNode = textNode;
 }
 
 // Fires whenever a choice button is pressed, choice is an integer which acts as an index for textNode.options[]
 function buttonPress(choice) {
   let nodeChoice = currentNode.options[choice];
-  
+
   textElement.innerText+="\n: "+letters[choice];
 
   if (nodeChoice === undefined) {
@@ -41,7 +42,7 @@ function buttonPress(choice) {
     console.log(letters[choice]+": >"+timeline);
 
     // Select how the game continues
-    // -1: Redo; 0: Game Over; 100: Next Trial
+    // -1: Redo; -2: Redo+Damage; 0: Game Over; 100: Next Trial
     switch (timeline) {
       case -2:
         health-=1;
@@ -60,9 +61,14 @@ function buttonPress(choice) {
         break;
       case 100:
         console.log("Next trial");
-        health = 3;
+        currentTrial++;
+        health = 5;
         textElement.innerText+="\nTRIAL COMPLETE\n\nHealth Restored!\n\n";
-        startTrial(trial2);
+        if (trials[currentTrial-1] == undefined) {
+          textElement.innerText+="\nGame undons't";
+        } else {
+          startTrial(trials[currentTrial-1]);
+        }
         break;
       default:
         ask(timeline);
@@ -83,6 +89,10 @@ function gameOver() {
 
 let trial1 = [{"id":1,"text":"Welcome home, Hercules. I see your adventures have not gone as expected. What is upsetting you? Hercules, please. Put down the knife.","options":[{"text":"Kill","result":"(Thunder sound plays) Title shows: The Twelve Labors of Hercules","next":2},{"text":"Dont kill","result":"Lame","next":0},{"text":"Someone explain to me how to make bold text in a form","result":"plsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplsplspls","next":-1}]},{"id":2,"text":"You appear on Mount Olympus, the home of the Gods.\nApollo: Welcome, Hercules. You have killed your family. How do you plead?","options":[{"text":"Guilty","result":"Wise choice. I will give you a simple punishment: Do twelve good deeds. After this, you will be forgiven.","next":3},{"text":"Not guilty","result":"You dare lie to a god, you foolish mortal. For this act, I will punish you severely. Do twelve good deeds. After this, maybe I will forgive you.","next":3}]},{"id":3,"text":"King Eurysthesus: Hercules! Please help us. A giant lion is attacking my poor citizens. We are unable to defeat it, it survives anything we use against it. Will you help us?","options":[{"text":"Yes, of course I will help","result":"King Eurysthesus: Thank you. The lion is just up ahead.\nYou approach the lion. It is very large, and would be destroying the city if not distracted by an equally large ball of twine.","next":4},{"text":"No. Screw you.","result":"Apollo: Hercules, do your labors or I will kill you.","next":-1}]},{"id":4,"text":"You approach the lion. It is very large, and would be destroying the city if not distracted by an equally large ball of twine.","options":[{"text":"Shoot the lion with arrows","result":"You shoot the arrows, but they bounce off the lion's hide. The lion retreats into a nearby cave.","next":5},{"text":"Say hello to the lion","result":"The lion scratches you.","next":-2}]},{"id":5,"text":"","options":[{"text":"Wait at the cave entrance","result":"The giant ball of twine rolls over and crushes you","next":-2},{"text":"Follow it into the cave","result":"You walk into the cave. It is a large space. Skeletons litter the floors. From further down the cave, you hear a roar.","next":6}]},{"id":6,"text":"","options":[{"text":"Exit the cave. Too scary.","result":"The giant ball of twine hits you in the face and sends you back into the cave.","next":-1},{"text":"Continue further.","result":"You walk through the cave. There is a dripping sound coming from somewhere. You see evidence of the lion’s path; bones on the floor, rocks moved over, the lion. Oh no.","next":7},{"text":"Wait here","result":"The giant ball of twine rolls into the cave and runs you over.","next":-2}]},{"id":7,"text":"","options":[{"text":"Hit the lion with your club.","result":"Your club does not affect the lion. It swats you with its paw.","next":-2},{"text":"Run.","result":"You run down the cave. The lion chases you. You fall down a chasm and land in a pool of water. The lion tumbles down the chasm after you.","next":8}]},{"id":8,"text":"","options":[{"text":"Pet the lion.","result":"The lion bites your hand.","next":-2},{"text":"Swim away.","result":"The lion swims after you and attacks","next":-2},{"text":"Jump on the lion’s back.","result":"You wrap your arms around the lion's neck and begin to choke it. It falls into the water, completely unable to breathe. You wait for it to stop moving, which it eventually does. The lion is dead.","next":9}]},{"id":9,"text":"","options":[{"text":"Pick up it's pelt","result":"You pick up the pelt. Somehow, it magically turned into a coat once the lion was killed. You don the coat and walk out of the cave. You go back to Nemea. The people cheer as you walk victoriously through the streets. You approach King Eurystheus.\nKing Eurystheus: Wow. You actually did it. I really thought that you would die. Dang, that’s impressive. Ha. Probably shouldn’t have said that. Ok goodbye Hercules please don’t hurt me I’m a frail old man I can’t die my people need me I’m sorry Hercules.","next":100},{"text":"Exit","result":"The sun shines. You exit the cave victorious. And then someone shoots you. Then, the giant ball of twine hits you and you fall all the way back through the cave. Should’ve taken the invulnerable pelt.","next":-1}]}];
 let trial2 = [{"id":1,"text":"King Eurystheus: Hello again Hercules. Nemea is once again under attack by another giant monster. Why does this keep happening. I mean, there are other kingdoms in Greece. Anyway, big snake monster thing. Go deal with it.\nYou travel to a familiar town, where the hydra was spotted nearby. You recognize this place. Your nephew lives here. You knock on his door.\nIolaus: Uncle Hercules! OH GOD WAIT NO PLEASE DON’T HURT ME PLEASE!\nHercules: I won’t hurt you. We need to work together to defeat the hydra.\nIolaus: Ok… I may be willing to help you. But I know what you did. You have a history of killing family members.","options":[{"text":"Kill him","result":"Hercules… Why? Let's pretend that didn’t happen.","next":2},{"text":"Advance on your quest.","result":"","next":2}]},{"id":2,"text":"You proceed on the quest. You hear leaves shuffle while you walk through the forest. You come across a stream. Suddenly, Iolaus screams. You turn around, and he is not there. You draw your club. Suddenly, a serpent head lunges out of the trees. Its jaw clamps on your chest… and does nothing. The pelt worked.","options":[{"text":"Chase it through the woods.","result":"","next":3},{"text":"Stay here.","result":"Somewhere in the distance, the giant ball of twine is loaded into a catapult. It is fired, and lands on you.","next":-2}]},{"id":3,"text":"You run after it. You hear Iolaus screaming. Suddenly, the hydra stops. Its nine heads stare at you. And then it throws Iolaus. He hits you and you both fall. You get up and help him to his feet.","options":[{"text":"Attack the hydra.","result":"","next":4},{"text":"Hug the hydra.","result":"The hydra is not a hugger.","next":-2}]},{"id":4,"text":"You bash its head in with your club. It is crushed, but two are regrown. You turn to Iolaus, and hand him a branch from the ground. Iolaus begins creating a fire.","options":[{"text":"Attack it again.","result":"","next":5},{"text":"Jump in the fire.","result":"Why would you do that? That’s kinda painful.","next":-2}]},{"id":5,"text":"You bash another one of its heads in. This time, Iolaus uses a torch to burn the spot where the head was. It does not regrow. You continue the process for another seven heads. There are only two remaining. The hydra lunges at you, and bites on you again. The cloak prevents it, but you are picked up in the air. You manage to bash one head while it lifts you, and Iolaus burns it. Iolaus takes the burning piece of wood and slices at the hydra’s last neck. You fall to the ground, still in its jaw. The head is no longer attached to the body.","options":[{"text":"Leave the hydra’s head.","result":"You leave the hydra’s head. Suddenly, Iolaus stabs you. What a jerk.","next":0},{"text":"Take the head.","result":"You pick up the hydra’s head. You carry it back to King Eurystheus.\nKing Eurystheus: Welcome back, Hercules, you’ve somehow survived again. You’re pretty good at this.","next":100}]}];
+let trial3 = [{"id":1,"text":"King Eurystheus: Ok Hercules, next I need you to go kill a deer for no reason. I am just in a murdery mood. It’s the deer of Diana, the Goddess of the hunt so… have fun I suppose.","options":[{"text":"Accuse him of running out of ideas.","result":"King Eurystheus: I am not out of ideas! You’re out of ideas!","next":-1},{"text":"Just go do it.","result":"","next":2}]},{"id":2,"text":"You walk off into the forest, see the deer, and shoot it with an arrow. Diana appears.\nDiana: Foolish mortal! You dare kill my beautiful pet. I will kill you for this!","options":[{"text":"Run","result":"","next":3},{"text":"Apologize","result":"You get shot with an arrow.","next":-2}]},{"id":3,"text":"You run. You sprint into the forest as dozens of arrows go by your head.","options":[{"text":"Continue running","result":"Diana shoots you.","next":-2},{"text":"Explain why you killed the deer.","result":"You explain that Apollo is making you do these labors.\nDiana: Yeah, that sounds like my brother. He’s an idiot.\nShe heals the deer, and allows you to bring it back to King Eurystheus. She also gives you a silver arrow.\nKing Eurystheus: Wow, you’re doing these tasks with incredible speed. S-S-S-Soon I will be reborn.\nHercules: What was that?\nKing Eurystheus: What? I said you were doing these tasks quickly.","next":100}]}];
+let trial4 = [{"id":1,"text":"You travel to Mount Erymanthus. You remember that your friend lives there, the centaur Pholus. You enter his cave, which is filled with centaurs.\nPholus: Hercules, ol’ buddy ol’ pal! How have you been? It’s been too long, my friend. Here, drink some wine.\nYou open the bottle of wine and pour yourself a glass.\nCentaur 1: Hey wait, he drank our wine.\nCentaur 2: I’ve never seen a centaur with two legs. Wait, he isn’t a centaur!\nThe horde of centaurs approach you. You turn to Pholus, but he is hidden behind the counter.","options":[{"text":"Attack the centaurs","result":"You hit one centaur over the head with your club. You take out your bow and shoot the silver arrow through another one. The arrow quickly returns to your hand. You begin using it as a melee weapon. All of the centaurs are dead.\nPholus: Wow, that is a cool arrow. Can I see it? This looks like it was built by Diana herself! And that enchantment to make it come back to your hand is…\nHe drops it on his hoof and dies instantly. You leave his bar. You use the glass of wine to lure the boar to King Eurystheus.","next":100},{"text":"Drink wine","result":"You don’t handle alcohol well, and now you can’t see straight.","next":-1}]}];
+
+let trials = [trial1, trial2, trial3, trial4];
 
 // Filled by startTrial, loads a single trial at a time
 let textNodes;
@@ -90,7 +100,7 @@ let textNodes;
 // Reads and loads a trial from JSON, resets timeline counter and starts a trial
 function startTrial(json) {
   console.log("Starting trial!")
-  
+
   // Loop through and parse each individual node
   for (let i=0; i<json.length; i++) {
     let node = json[i];
@@ -100,9 +110,10 @@ function startTrial(json) {
   }
   textNodes = json;
   timeline = 1;
-  
+  healthElement.innerText = "Health: "+health;
+
   let currentNode = textNodes.find(textNode => textNode.id === timeline);
-  
+
   ask(timeline);
 }
 
